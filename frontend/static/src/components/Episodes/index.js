4fa48b0b1264477e543785f2e5f5fecbe130adcd
Richard Summer <richardforrestsummer@gmail.com>
2020-11-14 22:34:25 -0500

    refactored episodes

--- frontend/static/src/components/Episodes/index.js
+++ frontend/static/src/components/Episodes/index.js
@@ -25,17 +25,19 @@ class Episodes extends Component {
     const episodesHTML = episodes.map(episode => <div key={episode.id}>{episode.name}</div>)
     return(
       <React.Fragment>
-        <div className="container" width="75%">
+        <div className="container" width="90%">
           <div className="row">
             <iframe src={`https://open.spotify.com/embed-podcast/episode/${this.state.currently_playing}`} title="player" width="100%" height="232" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
           </div>
           <div className="row">
-            <div className="col-8 episodes">
-                <h2>Episodes</h2>
-                {episodesHTML}
+            <div className="col-5 border border-right-0 rounded-left border-dark episodes">
+              <ul className="list-group">
+                  <li className="list-group-item list-group-item-primary">
+                    {episodesHTML}
+                  </li>
+              </ul>
             </div>
-            <div className="col-4 notes">
-              <h2>Notes</h2>
+            <div className="col-7 border border-left-0 rounded-right border-dark notes">
             </div>
           </div>
         </div>
